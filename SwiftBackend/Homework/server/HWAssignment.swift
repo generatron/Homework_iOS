fdaed050f2a9623ca829c0aadc9edafb29de1bc6
Evol Johnson <ejohnson@ngeosone.com>
2016-01-30 23:40:26 -0500

    Generated

---
+++ SwiftBackend/Homework/server/HWAssignment.swift
@@ -0,0 +1,58 @@
+/* 
+Copyright (c) 2016 NgeosOne LLC
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+   
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+ to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+Engineered using http://www.generatron.com/
+
+[GENERATRON]
+Generator :   System Templates
+Filename:     HWAssignment.swift
+Description:  Class that holds the data for HWAssignment
+Project:      Homework
+Template: /PerfectSwift/server/Entity.swift.vm
+ */
+
+
+import PerfectLib
+
+class HWAssignment : Entity {
+        var dateAssigned : NSDate
+        var dateDue : NSDate
+        var id : Int64
+        var isCompleted : Bool
+        var name : String
+        var type : String
+        
+    
+    func toDictionary() -> [String: Any] {
+                return [
+    "dateAssigned" : dateAssigned
+    ,"dateDue" : dateDue
+    ,"id" : id
+    ,"isCompleted" : isCompleted
+    ,"name" : name
+    ,"type" : type
+        ]
+    }
+}
+
+/* 
+[STATS]
+It would take a person typing  @ 100.0 cpm, 
+approximately 4.87 minutes to type the 487+ characters in this file.
+ */
+
+
