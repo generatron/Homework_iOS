64f13a8659bf1a25c0bfadb447fa28e702db0de6
Evol Johnson <ejohnson@ngeosone.com>
2016-01-31 17:13:43 -0500

    Cleanup

---
+++ SwiftBackend/HomeWork/HomeWork/HomeWork.h
@@ -0,0 +1,19 @@
+//
+//  HomeWork.h
+//  HomeWork
+//
+//  Created by Evol M Johnson on 1/31/16.
+//  Copyright © 2016 NgeosOne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HomeWork.
+FOUNDATION_EXPORT double HomeWorkVersionNumber;
+
+//! Project version string for HomeWork.
+FOUNDATION_EXPORT const unsigned char HomeWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HomeWork/PublicHeader.h>
+
+
